c25f1991ca2c89987381eb8a810e5ddfc112a63c
admin <admin@admni.com>
2022-05-12 12:28:35 +0300

    first commit

---
+++ client/src/pages/Contacts.js
@@ -0,0 +1,13 @@
+import Container from 'react-bootstrap/Container'
+
+const Contacts = () => {
+    return (
+        <Container>
+            <h1>Контакты</h1>
+            <h2> тут какой-то текст</h2>
+            <p> Выполнил Федоров Василий ИВТ-41-18</p>
+        </Container>
+    )
+}
+
+export default Contacts
